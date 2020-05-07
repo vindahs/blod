8ab5baa0589438915de01ce4589890cfcd82c0e8
vindahs <vincentonoja@ymail.com>
2020-05-07 18:00:22 +0100

    first commit

---
+++ blog/urls.py
@@ -0,0 +1,18 @@
+from . import views
+from django.urls import include
+
+from django.urls import path
+from .feeds import LatestPostsFeed, AtomSiteNewsFeed
+from .views import AboutPageView
+
+urlpatterns = [
+    path("feed/rss", LatestPostsFeed(), name="post_feed"),
+    path("feed/atom", AtomSiteNewsFeed()),
+    path("", views.PostList.as_view(), name="home"),
+    # path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path("<slug:slug>/", views.post_detail, name="post_detail"),
+     path("/", AboutPageView.as_view(), name='about'),
+
+
+]
+
